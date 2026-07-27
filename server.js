87ab6336992dda26938e8cd423bc12acb31a5225
utomo.harun@gmail.com <utomo.harun@gmail.com>
2026-07-27 22:02:51 +0700

    clean server v2

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Jalur simulasi saat aplikasi game WoS ...
+app.get('/api/player/profile', (req, res) => {
+    console.log("Aplikasi game WoS sedang ...");
+
+    // Di sini kita kunci data agar akun ...
+    const dataPemainTiruan = {
+        playerId: "WOS-FREE-VIP12",
+        playerName: "Developer Server",
+        diamonds: 9999999,
+        vipLevel: 12, // Mengunci status ...
+        allianceName: "Private Server Cre..."
+    };
+
+    res.json(dataPemainTiruan);
+});
+
+// Menyalakan server lokal di laptop HP A...
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server lokal WoS buatan ...`);
+});
